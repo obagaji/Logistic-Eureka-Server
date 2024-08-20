aca73a2ac6989fa9ce6fc601426ed80f470673ad
Musa Daniel <jtccontractsservicesng@yahoo.com>
2024-08-20 02:09:12 +0100

    Logistic Eureka server application for Microservice Discovery

---
+++ src/test/java/com/jtc/LogisticEurekaServer/LogisticEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtc.LogisticEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
